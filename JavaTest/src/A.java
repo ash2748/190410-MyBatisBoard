215e07a28693f4ae0744b25ec87cf57317a8c306
ash2748 <max3535@naver.com>
2019-04-22 14:21:50 +0900

    board,testProject에 databaseSQL 추가(190422,ash)

---
+++ JavaTest/src/A.java
@@ -0,0 +1,13 @@
+
+public class A {
+	public static void main(String[] args) {
+		
+		
+		Integer a = new Integer(5);
+		int b = 5;
+		Integer c = (Integer)b;
+		int d = (int)a;
+		System.out.println("C:"+c);
+		System.out.println(d);
+	}
+}
---
+++ JavaTest/src/B.java
@@ -0,0 +1,7 @@
+
+public class B {
+	public static int b(int a) {
+		return a = a+1;
+	}
+	
+}
